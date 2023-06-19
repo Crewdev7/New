9b5c0eb82a41737d311c2c5791a0cf4d2158fda0
crew <crewgamer45@gmail.com>
2023-06-19 22:39:08 +0530

    added password adding screen and 90% functionality

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:learningapp/main.dart';
+import 'package:mypass/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
